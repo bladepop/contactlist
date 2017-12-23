abd32eb4a836b136c60e9afe11bf224cde791a88
Daniel <danielbb124@gmail.com>
2017-12-23 14:55:20 +0200

    Added icon to search bar and animation

--- app/components/SearchBar/index.js
+++ app/components/SearchBar/index.js
@@ -10,13 +10,21 @@ import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+import SearchIcon from './search-icon.png';
+
 const SearchBarInput = styled.input`
-  background: white;
   border-radius: 30px;
   padding: 8px 10px;
   width: 200px;
   min-width: 100px;
   font-size: 0.75em;
+  transition: width 0.2s linear;
+  
+  background: white url(${SearchIcon}) no-repeat right 10px top 5px/20px 20px;
+  
+  &:focus {
+    width: 210px;
+  }
   
   ::placeholder {
     color:  ${(props) => props.theme.placeholderColor};
