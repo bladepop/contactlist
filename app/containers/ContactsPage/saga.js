cd9dbf6800d92f1aa3b7a9f31672a63b06e85e98
Daniel <danielbb124@gmail.com>
2017-12-23 19:16:11 +0200

    Added api response sample

--- app/containers/ContactsPage/saga.js
+++ app/containers/ContactsPage/saga.js
@@ -16,6 +16,17 @@ function ContactsException(message) {
 
 function* loadContactsSaga() {
   try {
+    /*
+      Response comes in the form of an an array of drivers:
+      [{
+        "name":"Juan Barrie",
+        "driverType":"Professional",
+        "driverRank":"5",
+        "phone":"123.456.7893",
+        "email":"driver.juan@gmail.com",
+        "profile_image":"http://images.unsplash.com/photo-1504575070132-986227df99f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=7ecf0d8a55890fdb50eba8d4def9645d"
+      }, ...]
+    */
     const response = yield call(fetch, CONTACT_LIST_URL, { type: 'GET', dataType: 'jsonp' });
     if (response.status !== 200) throw new ContactsException('Failed fetching contacts');
     const contacts = yield response.json();
